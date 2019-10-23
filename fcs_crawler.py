ee371f7bbab2a2109f249b2f29aadbdd94b43255
Stefan <stefam1996@live.com>
2019-10-23 23:50:23 +0200

    added function to remove html attributes

--- fcs_crawler.py
+++ fcs_crawler.py
@@ -1,11 +1,21 @@
 from utils import get_request, to_soup
 
+def remove_attributes(content):
+	content = to_soup(str(content))
+	ATTRS_TO_REMOVE = ["srcset", "alt", "id", "class", "height", "width", "sizes"]
+	for attribute in ATTRS_TO_REMOVE:
+		for tag in content.find_all(attrs={attribute: True}):
+			del tag[attribute]
+	return content
+
 def content_scraper(link):
 	request = get_request(link)
 	soup = to_soup(request)
+
 	main_content = soup.find("div", {"id": "inner-block"})
-	main_content = str(main_content.find("img", {"class", "attachment-medium-feature size-medium-feature wp-post-image"}))
-	main_content = main_content.replace('\n', '<br />')
+	main_content = main_content.find("img", {"class", "attachment-medium-feature size-medium-feature wp-post-image"})
+	main_content = remove_attributes(main_content)
+	main_content = str(main_content).replace('\n', '<br />')
 	return main_content
 
 def news_crawler(soup):
@@ -38,3 +48,7 @@ def fcs_crawler_main():
 	soup = to_soup(request)
 	news_json_list = news_crawler(soup)
 	return news_json_list
+
+
+if __name__ == '__main__':
+	fcs_crawler_main()
